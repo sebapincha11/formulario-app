7804240afe462fe34e32bffd3b80ade552428518
Sebastian <seba.faingerch@gmail.com>
2023-09-28 20:56:47 -0300

    FEAT: index style finish

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,12 +3,32 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Formulario
-    </title>
+    <title>Formulario</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
   </head>
   <body>
-    <h1>Hello, world!</h1>
+    
+    <div class="d-flex justify-content-center align-items-center" style="height: 100vh;">
+        <div>
+            <div class="form-floating mt-3 mb-3">
+                <input type="text" class="form-control" id="name" placeholder="Name">
+                <label for="text">Name</label>
+            </div>
+            <div class="form-floating mt-3 mb-3">
+                <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+                <label for="floatingInput">Email address</label>
+            </div>
+            <div class="form-floating">
+                <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea2" style="height: 100px"></textarea>
+                <label for="floatingTextarea2">Comments</label>
+            </div>
+            <div class="form-floating mt-3">
+                <button type="submit" id="btnSend" class="btn btn-primary">Enviar</button>
+            </div>
+        </div>
+    </div>
+    
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
   </body>
 </html>
